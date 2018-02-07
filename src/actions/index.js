16b43ac035f568f8f51b98d9c9ee018da95e925b
ngo lam <ngolam>
2018-02-07 08:51:42 +0700

    middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,9 @@
 const API_KEY = '815025e25ae254eeb4185dc303aec42d';
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather() {
+  return {
+    type: FETCH_WEATHER
+  };
+}
