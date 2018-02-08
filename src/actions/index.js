520385fc95c31b00531005f223049f00f08affa8
ngo lam <ngolam>
2018-02-08 08:10:27 +0700

    adfkd

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,15 @@
+import axios from 'axios';
 const API_KEY = '815025e25ae254eeb4185dc303aec42d';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather() {
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
